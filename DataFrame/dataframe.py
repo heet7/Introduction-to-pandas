1d9fdc680165e192b30f4e60c40fb0685de1d7e4
heet bhalala <44464602+heet7@users.noreply.github.com>
2021-08-19 14:22:01 +0530

    Update dataframe.py

--- DataFrame/dataframe.py
+++ DataFrame/dataframe.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+# create a set
+a = {"name":["Heet","Jash","Raxit"],
+     "age" :[ 22   ,  19  ,  21  ]
+    }
+
+# convert into data frame
+b = pd.DataFrame(a)
+
+# set default name of the rows
+pd.DataFrame(a ,index = ['r1','r2','r3']) #index = that equal value of name only then it change otherwise it gives error...
+
+# change name of the columns
+b.columns =['c1','c2']
+b
